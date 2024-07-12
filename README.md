38e34acd087c66df1fe64817ed84469f75fd2b25
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-07-11 21:43:04 -0300

    root

---
+++ README.md
@@ -0,0 +1,2 @@
+# Your wish is my command
+This application intents to be a place for artisans, craftsman and people who look for this kind of job.
---
+++ src/main/java/br/com/yourwishismycommand/YourWishIsMyCommandApplication.java
@@ -0,0 +1,13 @@
+package br.com.yourwishismycommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourWishIsMyCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourWishIsMyCommandApplication.class, args);
+    }
+
+}
