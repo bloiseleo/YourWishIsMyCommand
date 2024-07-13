b1f328c73afef2b278f39e0a3a531622540e5128
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-07-13 18:01:15 -0300

    - Changing response from Controller

--- src/main/java/br/com/yourwishismycommand/infra/controllers/AuthController.java
+++ src/main/java/br/com/yourwishismycommand/infra/controllers/AuthController.java
@@ -1,21 +1,26 @@
 package br.com.yourwishismycommand.infra.controllers;
 
+import br.com.yourwishismycommand.application.dtos.APIBaseResponse;
 import br.com.yourwishismycommand.application.usecases.RegisterUserUseCase;
 import br.com.yourwishismycommand.infra.dtos.UserDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import static br.com.yourwishismycommand.infra.HttpResponseHelper.created;
 @RestController
 @RequestMapping("auth")
 public class AuthController {
-    @Autowired
-    private RegisterUserUseCase registerUserUseCase;
+    private final RegisterUserUseCase registerUserUseCase;
+    public AuthController(RegisterUserUseCase   registerUserUseCase) {
+        this.registerUserUseCase = registerUserUseCase;
+    }
     @PostMapping("register")
     @ResponseStatus(HttpStatus.CREATED)
-    public String register(@RequestBody UserDTO createUserDTO) {
+    public ResponseEntity<APIBaseResponse> register(@RequestBody UserDTO createUserDTO) {
         var user = registerUserUseCase.registerUser(createUserDTO);
-        return "olá";
+        return created(
+                String.format("User of email %s created successfully", user.getEmail())
+        );
     }
 }
