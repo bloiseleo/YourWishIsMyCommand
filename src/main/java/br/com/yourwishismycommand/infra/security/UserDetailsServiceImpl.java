352e4fe1918bf7b63cd0208e5a85fb20a6d36130
Leonardo Bloise <leonardocsbloise@gmail.com>
2024-07-20 13:06:02 -0300

    Implementing the custom class in order to retrieve it.

--- src/main/java/br/com/yourwishismycommand/infra/security/UserDetailsServiceImpl.java
+++ src/main/java/br/com/yourwishismycommand/infra/security/UserDetailsServiceImpl.java
@@ -3,37 +3,17 @@ package br.com.yourwishismycommand.infra.security;
 import br.com.yourwishismycommand.domain.entities.Email;
 import br.com.yourwishismycommand.domain.entities.User;
 import br.com.yourwishismycommand.domain.repositories.UserRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Collection;
-import java.util.List;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    public static UserDetails adapt(User user) {
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return List.of(new SimpleGrantedAuthority(user.getRole().name()));
-            }
-
-            @Override
-            public String getPassword() {
-                return user.getPassword();
-            }
-
-            @Override
-            public String getUsername() {
-                return user.getEmail().toString();
-            }
-        };
+    public static UserDetailsImpl adapt(User user) {
+        return new UserDetailsImpl(user);
     }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
